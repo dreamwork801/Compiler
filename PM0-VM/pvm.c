67752f3102d0ea6c130904446cb9233511c584f4
Zach Colby <zachary@zacharycolby.com>
2015-05-27 14:53:55 -0400

    Refactor work

--- PM0-VM/pvm.c
+++ PM0-VM/pvm.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 #define MAX_STACK_HEIGHT 2000
-#define MAX_CODE_LENGTH 17 //CHANGE THIS LATER BACK TO 500!!!!!!!!!!!!!!!
+#define MAX_CODE_LENGTH 500
 #define MAX_LEXI_LEVELS 3
 
 struct Instruction {
@@ -16,7 +16,9 @@ struct Instruction {
 struct Instruction code[MAX_CODE_LENGTH];
 struct Instruction ir;
 int stack[MAX_STACK_HEIGHT];
+int sp,bp,pc = 0;
 
+//Gets the optext for the given opcode
 char* getOpText(int opcode){
     char *optext;
     switch (opcode) {
@@ -57,7 +59,7 @@ char* getOpText(int opcode){
 int main()
 {
     // index, stack pointer, base pointer, pc
-    int i,sp,bp,pc,numInstructions,halt = 0;
+    int i,numInstructions,halt = 0;
 
     ir.op = 0;
     ir.l = 0;
@@ -77,16 +79,17 @@ int main()
 
     //Read from files
     for (i = 0; i<MAX_CODE_LENGTH; i++){
-        fscanf(fpr, "%d", &code[i].op);
+        if (fscanf(fpr, "%d", &code[i].op) == EOF){
+            break;
+        }
         fscanf(fpr, "%d", &code[i].l);
         fscanf(fpr, "%d", &code[i].m);
     }
-    //This the number of instructions given in this file NEEDS FIXING!!!!!!!!!!!!!!!!!!!
     numInstructions = i;
 
     fprintf(fpw, "Line\tOP\tL\tM\n");
 
-    // Print out the input
+    // Write the input to file
     for (i=0; i<numInstructions; i++){
         fprintf(fpw, "%d\t", i);
         fprintf(fpw, "%s\t", getOpText(code[i].op));
@@ -104,123 +107,14 @@ int main()
     fprintf(fpw, "Initial values\t\t\t%d\t%d\t%d\n",pc,bp,sp);
 
     while (halt == 0){
-    //for (i = 0; i<10; i++){
         ir.l = code[pc].l;
         ir.op = code[pc].op;
         ir.m = code[pc].m;
         fprintf(fpw, "%d\t", pc);
         pc++;
 
-        //printf("%d ", code[pc].op);
+        halt = execute(halt);
 
-        switch (ir.op) {
-            case 1: //LIT
-                sp = sp+1;
-                stack[sp] = ir.m;
-                break;
-            case 2: //OPR
-                switch (ir.m){
-                    case 0: //RET
-                        sp = bp - 1;
-                        pc = stack[sp + 4];
-                        bp = stack[sp + 3];
-                        break;
-                    case 1: //NEG
-                        stack[sp] = -stack[sp];
-                        break;
-                    case 2: //ADD
-                        sp = sp - 1;
-                        stack[sp] = stack[sp] + stack[sp + 1];
-                        break;
-                    case 3: //SUB
-                        sp = sp - 1;
-                        stack[sp] = stack[sp] - stack[sp + 1];
-                        break;
-                    case 4: //MUL
-                        sp = sp - 1;
-                        stack[sp] = stack[sp] * stack[sp + 1];
-                        break;
-                    case 5: //DIV
-                        sp = sp - 1;
-                        stack[sp] = stack[sp] / stack[sp + 1];
-                        break;
-                    case 6: //ODD
-                        stack[sp] = stack[sp] % 2;
-                        break;
-                    case 7: //MOD
-                        sp = sp - 1;
-                        stack[sp] = stack[sp] % stack[sp + 1];
-                        break;
-                    case 8: //EQL
-                        sp = sp - 1;
-                        stack[sp] = (stack[sp] == stack[sp + 1]);
-                        break;
-                    case 9: //NEQ
-                        sp = sp - 1;
-                        stack[sp] = (stack[sp] != stack[sp + 1]);
-                        break;
-                    case 10: //LSS
-                        sp = sp - 1;
-                        stack[sp] = (stack[sp] < stack[sp + 1]);
-                        break;
-                    case 11: //LEQ
-                        sp = sp - 1;
-                        stack[sp] = (stack[sp] <= stack[sp + 1]);
-                        break;
-                    case 12: //GTR
-                        sp = sp - 1;
-                        stack[sp] = (stack[sp] > stack[sp + 1]);
-                        break;
-                    case 13: //GEQ
-                        sp = sp - 1;
-                        stack[sp] = (stack[sp] >= stack[sp + 1]);
-                        break;
-                }
-                break;
-            case 3: //LOD
-                sp = sp + 1;
-                stack[sp] = stack[base(ir.l, bp) + ir.m];
-                break;
-            case 4: //STO
-                stack[base(ir.l, bp) + ir.m] = stack[sp];
-                sp = sp - 1;
-                break;
-            case 5: //CAL
-                stack[sp + 1] = 0;              //return value (FV)
-                stack[sp + 2] = base(ir.l, bp);    //static link (SL)
-                stack[sp + 3] = bp;             //dynamic link {DL)
-                stack[sp + 4] = pc;             //return address (RA)
-                bp = sp + 1;
-                pc = ir.m;
-                break;
-            case 6: //INC
-                sp = sp + ir.m;
-                break;
-            case 7: //JMP
-                pc = ir.m;
-                break;
-            case 8: //JPC
-                if (stack [sp] == 0){
-                    pc = ir.m;
-                }
-                sp = sp - 1;
-                break;
-            case 9: // SIO
-                switch (ir.m){
-                    case 0:
-                        printf("%d", stack [sp]);
-                        sp = sp - 1;
-                        break;
-                    case 1:
-                        sp = sp + 1;
-                        scanf ("%d", &stack[sp]);
-                        break;
-                    case 2:
-                        halt = 1;
-                        break;
-                } //end SIO switch
-                break;
-        } // end ir.op switch
         fprintf(fpw, "%s\t%d\t%d\t%d\t%d\t%d\t", getOpText(ir.op), ir.l, ir.m, pc, bp, sp);
         for (i=1; i<=sp; i++){
             fprintf(fpw, "%d ", stack[i]);
@@ -241,4 +135,116 @@ int base(int level, int b){
         level--;
     }
     return b;
+}
+
+int execute(int halt){
+    switch (ir.op) {
+        case 1: //LIT
+            sp = sp+1;
+            stack[sp] = ir.m;
+            break;
+        case 2: //OPR
+            switch (ir.m){
+                case 0: //RET
+                    sp = bp - 1;
+                    pc = stack[sp + 4];
+                    bp = stack[sp + 3];
+                    break;
+                case 1: //NEG
+                    stack[sp] = -stack[sp];
+                    break;
+                case 2: //ADD
+                    sp = sp - 1;
+                    stack[sp] = stack[sp] + stack[sp + 1];
+                    break;
+                case 3: //SUB
+                    sp = sp - 1;
+                    stack[sp] = stack[sp] - stack[sp + 1];
+                    break;
+                case 4: //MUL
+                    sp = sp - 1;
+                    stack[sp] = stack[sp] * stack[sp + 1];
+                    break;
+                case 5: //DIV
+                    sp = sp - 1;
+                    stack[sp] = stack[sp] / stack[sp + 1];
+                    break;
+                case 6: //ODD
+                    stack[sp] = stack[sp] % 2;
+                    break;
+                case 7: //MOD
+                    sp = sp - 1;
+                    stack[sp] = stack[sp] % stack[sp + 1];
+                    break;
+                case 8: //EQL
+                    sp = sp - 1;
+                    stack[sp] = (stack[sp] == stack[sp + 1]);
+                    break;
+                case 9: //NEQ
+                    sp = sp - 1;
+                    stack[sp] = (stack[sp] != stack[sp + 1]);
+                    break;
+                case 10: //LSS
+                    sp = sp - 1;
+                    stack[sp] = (stack[sp] < stack[sp + 1]);
+                    break;
+                case 11: //LEQ
+                    sp = sp - 1;
+                    stack[sp] = (stack[sp] <= stack[sp + 1]);
+                    break;
+                case 12: //GTR
+                    sp = sp - 1;
+                    stack[sp] = (stack[sp] > stack[sp + 1]);
+                    break;
+                case 13: //GEQ
+                    sp = sp - 1;
+                    stack[sp] = (stack[sp] >= stack[sp + 1]);
+                    break;
+            }
+            break;
+        case 3: //LOD
+            sp = sp + 1;
+            stack[sp] = stack[base(ir.l, bp) + ir.m];
+            break;
+        case 4: //STO
+            stack[base(ir.l, bp) + ir.m] = stack[sp];
+            sp = sp - 1;
+            break;
+        case 5: //CAL
+            stack[sp + 1] = 0;              //return value (FV)
+            stack[sp + 2] = base(ir.l, bp);    //static link (SL)
+            stack[sp + 3] = bp;             //dynamic link {DL)
+            stack[sp + 4] = pc;             //return address (RA)
+            bp = sp + 1;
+            pc = ir.m;
+            break;
+        case 6: //INC
+            sp = sp + ir.m;
+            break;
+        case 7: //JMP
+            pc = ir.m;
+            break;
+        case 8: //JPC
+            if (stack [sp] == 0){
+                pc = ir.m;
+            }
+            sp = sp - 1;
+            break;
+        case 9: // SIO
+            switch (ir.m){
+                case 0:
+                    printf("%d", stack [sp]);
+                    sp = sp - 1;
+                    break;
+                case 1:
+                    sp = sp + 1;
+                    scanf ("%d", &stack[sp]);
+                    break;
+                case 2:
+                    halt = 1;
+                    break;
+            } //end SIO switch
+            break;
+    } // end ir.op switch
+    return halt;
 }
